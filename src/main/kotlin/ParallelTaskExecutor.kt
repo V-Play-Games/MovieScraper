352efddee1b818aaec9bfcebec3dc02649944292
V Play Games <vaibhavnargwani28@gmail.com>
2025-07-03 17:30:51 +0530

    Add many helping functions

--- src/main/kotlin/ParallelTaskExecutor.kt
+++ src/main/kotlin/ParallelTaskExecutor.kt
@@ -5,6 +5,8 @@ import java.net.URI
 import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicInteger
 
+val baseScrapeCacheDir = File("D:/scrape-cache")
+
 suspend fun <T, R> List<T>.executeTask(
     taskIdentifier: String,
     threadCount: Int = Runtime.getRuntime().availableProcessors(),
@@ -18,17 +20,20 @@ suspend fun <T, R> List<T>.executeTask(
         val header = "Task: $taskIdentifier | Count: $totalTasks | Threads: $threadCount"
 
         val progressChannel = Channel<Int>(Channel.BUFFERED)
-        val completedCounter = AtomicInteger(0)
+        val successCounter = AtomicInteger(0)
+        val errorCounter = AtomicInteger(0)
         // Launch progress tracking coroutine if needed
         val progressJob = launch {
             for (update in progressChannel) {
-                val completed = completedCounter.incrementAndGet()
+                val success = successCounter.get()
+                val error = errorCounter.get()
+                val completed = success + error
                 val percent = completed * 100 / totalTasks
                 val completeWidth = progressBarWidth * percent / 100
                 val remainingWidth = progressBarWidth - completeWidth
                 val bar = "[" + "=".repeat(completeWidth) + " ".repeat(remainingWidth) + "]"
 
-                print("\r$header | Progress: $bar $percent%")
+                print("\r$header | Progress: $bar $percent% | Success: $success | Errors: $error")
             }
         }
 
@@ -39,9 +44,11 @@ suspend fun <T, R> List<T>.executeTask(
                 async {
                     try {
                         val result = taskProcessor(task)
+                        successCounter.incrementAndGet()
                         progressChannel.send(1)
                         TaskResult.Success(task, result)
                     } catch (e: Exception) {
+                        errorCounter.incrementAndGet()
                         progressChannel.send(1)
                         TaskResult.Failure(task, e)
                     }
@@ -65,30 +72,96 @@ suspend fun <T, R> List<T>.executeTask(
     }
 }
 
+suspend fun <T, R1, R2> List<TaskResult<T, R1>>.executeTask(
+    taskIdentifier: String,
+    threadCount: Int = Runtime.getRuntime().availableProcessors(),
+    progressBarWidth: Int = 50,
+    taskProcessor: suspend (T, R1) -> R2
+): List<TaskResult<T, R2>> = this
+    .executeTask(taskIdentifier, threadCount, progressBarWidth) { result ->
+        when (result) {
+            is TaskResult.Success -> taskProcessor.invoke(result.task, result.result)
+            is TaskResult.Failure -> throw result.error
+        }
+    }.mapResults()
+
+fun <T, R1, R2> List<TaskResult<TaskResult<T, R1>, R2>>.mapResults(): List<TaskResult<T, R2>> =
+    this.map { result: TaskResult<TaskResult<T, R1>, R2> ->
+        when (result) {
+            is TaskResult.Success -> when (result.task) {
+                is TaskResult.Success -> TaskResult.Success(result.task.task, result.result)
+                is TaskResult.Failure -> TaskResult.Failure(result.task.task, result.task.error)
+            }
+
+            is TaskResult.Failure -> when (result.task) {
+                is TaskResult.Success -> TaskResult.Failure(result.task.task, result.error)
+                is TaskResult.Failure -> TaskResult.Failure(result.task.task, result.task.error)
+            }
+        }
+    }
+
+
 suspend fun <T> List<T>.executeScrapeTask(
     taskIdentifier: String,
-    cacheFolderName: String,
     threadCount: Int = Runtime.getRuntime().availableProcessors(),
     progressBarWidth: Int = 50,
     urlFileProcessor: suspend (T) -> Pair<String, String>
 ): List<TaskResult<T, File>> {
-    val cacheFolder = File("scrape-cache", cacheFolderName).apply {
-        if (!exists()) mkdirs()
-    }
     return this.executeTask(taskIdentifier, threadCount, progressBarWidth) { task ->
         val (url, fileName) = urlFileProcessor(task)
-        val file = File(cacheFolder, fileName)
+        val file = File(baseScrapeCacheDir, fileName)
         if (!file.exists()) {
-            file.writeText(URI(url).toURL().readText())
+            val text = URI(url).toURL().readText()
+            file.parentFile.mkdirs()
+            file.writeText(text)
         }
         return@executeTask file
     }
 }
 
-fun <T, R> List<TaskResult<T, R>>.filterSuccessful() = filterIsInstance<TaskResult.Success<T, R>>()
+suspend fun <T, R> List<TaskResult<T, R>>.executeScrapeTask(
+    taskIdentifier: String,
+    threadCount: Int = Runtime.getRuntime().availableProcessors(),
+    progressBarWidth: Int = 50,
+    urlFileProcessor: suspend (T, R) -> Pair<String, String>
+) = this
+    .executeScrapeTask(taskIdentifier, threadCount, progressBarWidth) { result ->
+        when (result) {
+            is TaskResult.Success -> urlFileProcessor.invoke(result.task, result.result)
+            is TaskResult.Failure -> throw result.error
+        }
+    }.mapResults()
 
-sealed class TaskResult<T, R> {
-    data class Success<T, R>(val task: T, val result: R) : TaskResult<T, R>()
+fun <T, R> List<TaskResult<T, List<R>>>.flattenTaskResults(): List<TaskResult<T, R>> =
+    this.flatMap { taskResult ->
+        when (taskResult) {
+            is TaskResult.Success -> taskResult.result.map { result -> TaskResult.Success(taskResult.task, result) }
+            is TaskResult.Failure -> listOf(TaskResult.Failure(taskResult.task, taskResult.error))
+        }
+    }
 
-    data class Failure<T, R>(val task: T, val error: Exception) : TaskResult<T, R>()
+fun <T, R1, R2> List<TaskResult<T, R1>>.mapResults(transform: (R1) -> R2): List<TaskResult<T, R2>> =
+    this.map {
+        when (it) {
+            is TaskResult.Success -> TaskResult.Success(it.task, transform(it.result))
+            is TaskResult.Failure -> TaskResult.Failure(it.task, it.error)
+        }
+    }
+
+fun <T, R> List<TaskResult<T, R>>.filterSuccessful() =
+    this.mapNotNull { taskResult ->
+        when (taskResult) {
+            is TaskResult.Success -> taskResult
+            is TaskResult.Failure -> null.also {
+                println("Task failed: ${taskResult.task} - Error: ${taskResult.error.message}")
+            }
+        }
+    }
+
+fun <T, R> List<TaskResult<T, R>>.mapToResult() = filterSuccessful().map { it.result }
+
+sealed class TaskResult<T, R>(open val task: T) {
+    data class Success<T, R>(override val task: T, val result: R) : TaskResult<T, R>(task)
+
+    data class Failure<T, R>(override val task: T, val error: Exception) : TaskResult<T, R>(task)
 }
